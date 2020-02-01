a828ebeab88a668e9e66a6a32179fc0a7f90c82c
邱子昂 <skylove@MacBook-Pro.local>
2020-02-01 13:37:17 +0100

    我的修改

--- WeiboTopicScrapy.py
+++ WeiboTopicScrapy.py
@@ -1,12 +1,4 @@
 # -*- coding: utf-8 -*-
-# author:           inspurer(月小水长)
-# pc_type           lenovo
-# create_time:      2020/1/12 22:04
-# file_name:        main.py
-# github            https://github.com/inspurer
-# qq邮箱            2391527690@qq.com
-# 微信公众号         月小水长(ID: inspurer)
-
 
 import requests
 
